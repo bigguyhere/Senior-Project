a7b72e935ce689ea13b51517bbb5fd51346188ec
Liam <lkpowp@gmail.com>
2018-09-26 16:31:12 -0400

    Commit Update 1

--- Senior-Project/src/VN_GUI.java
+++ Senior-Project/src/VN_GUI.java
@@ -1,5 +1,12 @@
+import javax.swing.JFrame;
 
 public class VN_GUI 
 {
-
+	public static void main(String[] args)
+	{
+		JFrame mainWindow = new JFrame("Visual Novel Creation Tool");
+		mainWindow.setVisible(true);
+		mainWindow.setSize(1920, 1080);
+	}
+	
 }
