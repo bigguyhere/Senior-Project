978d851010e73a517067d8fdfc0f9bec835af05e
Liam <lkpowp@gmail.com>
2018-09-26 19:38:42 -0400

    Commit Update 3

--- Senior-Project/src/VN_GUI.java
+++ Senior-Project/src/VN_GUI.java
@@ -1,23 +1,42 @@
+// Authors: Liam Kastell & Sean Beckerle
 import java.awt.event.*;
+import java.awt.Toolkit;
 import javax.swing.*;
 
 public class VN_GUI 
 {
 	public static void main(String[] args)
 	{
+		VN_GUI.start();
+	}
+	
+	public static void start()
+	{
 		JFrame mainWindow = new JFrame("Visual Novel Engine");
+		JMenu file = new JMenu("File"); //Menu Items
+		JMenuItem newProject = new JMenuItem("New"); //Menu SubItems
+		JMenuItem save = new JMenuItem("Save");
+		JMenuBar menuBar = new JMenuBar(); //Creates Menu Bar
 		
-		JMenu menu = new JMenu("File");
-		menu.setMnemonic(KeyEvent.VK_F);
-		JMenuBar menuBar = new JMenuBar();
+		//Allows user to use shortcuts
+		file.setMnemonic(KeyEvent.VK_F); //Alt-F
+		newProject.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); //Ctrl N
+		save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl S
 		
-		menuBar.add(menu);
+		//Adds SubItems to Menu
+		file.add(newProject);
+		file.add(save);
 		
+		
+		
+		//Adds Menu to MenuBar
+		menuBar.add(file);
+		
+		//Creates the main window
 		mainWindow.setJMenuBar(menuBar);
 		mainWindow.setSize(1920, 1080);
 		
 		mainWindow.setVisible(true);
-		
 	}
 	
 }
