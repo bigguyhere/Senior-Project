f19e14d7c445de52c990117637b87579199aa0d3
Liam <lkpowp@gmail.com>
2018-09-26 17:45:46 -0400

    Commit Update 2

--- Senior-Project/src/VN_GUI.java
+++ Senior-Project/src/VN_GUI.java
@@ -1,12 +1,23 @@
-import javax.swing.JFrame;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class VN_GUI 
 {
 	public static void main(String[] args)
 	{
-		JFrame mainWindow = new JFrame("Visual Novel Creation Tool");
-		mainWindow.setVisible(true);
+		JFrame mainWindow = new JFrame("Visual Novel Engine");
+		
+		JMenu menu = new JMenu("File");
+		menu.setMnemonic(KeyEvent.VK_F);
+		JMenuBar menuBar = new JMenuBar();
+		
+		menuBar.add(menu);
+		
+		mainWindow.setJMenuBar(menuBar);
 		mainWindow.setSize(1920, 1080);
+		
+		mainWindow.setVisible(true);
+		
 	}
 	
 }
